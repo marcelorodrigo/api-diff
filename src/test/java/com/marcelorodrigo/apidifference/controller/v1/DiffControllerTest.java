5ac0e06e2748140bf076810ae9541920c4c9f607
Marcelo Rodrigo <mrodrigow@gmail.com>
2021-05-06 15:53:30 +0200

    JUnit 5

--- src/test/java/com/marcelorodrigo/apidifference/controller/v1/DiffControllerTest.java
+++ src/test/java/com/marcelorodrigo/apidifference/controller/v1/DiffControllerTest.java
@@ -3,20 +3,20 @@ package com.marcelorodrigo.apidifference.controller.v1;
 import com.marcelorodrigo.apidifference.model.ResultType;
 import com.marcelorodrigo.apidifference.vo.Base64VO;
 import com.marcelorodrigo.apidifference.vo.DiffResult;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DiffControllerTest {
 
--- src/test/java/com/marcelorodrigo/apidifference/util/Base64UtilTest.java
+++ src/test/java/com/marcelorodrigo/apidifference/util/Base64UtilTest.java
@@ -1,22 +1,23 @@
 package com.marcelorodrigo.apidifference.util;
 
 import com.marcelorodrigo.apidifference.exception.InvalidBase64Exception;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class Base64UtilTest {
 
     @Test
     public void decodeValidBase64() throws Exception {
-        String originalData = "WAES Assignment\n2019";
-        String encodedBase64 = "V0FFUyBBc3NpZ25tZW50CjIwMTk=";
+        final String originalData = "WAES Assignment\n2019";
+        final String encodedBase64 = "V0FFUyBBc3NpZ25tZW50CjIwMTk=";
         assertEquals(originalData, Base64Util.decode(encodedBase64));
     }
 
-    @Test(expected = InvalidBase64Exception.class)
-    public void decodeInvalidBase64() throws Exception {
-        String invalidData = "SoM3InvaLidD@ta=";
-        Base64Util.decode(invalidData);
+    @Test
+    public void decodeInvalidBase64() {
+        final String invalidData = "SoM3InvaLidD@ta=";
+        assertThrows(InvalidBase64Exception.class, () -> Base64Util.decode(invalidData));
     }
 }
