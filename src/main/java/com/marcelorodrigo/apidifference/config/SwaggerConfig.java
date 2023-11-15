bea4a79a5ebc73051fbc4dbb9af8eb52f2d15ab7
Marcelo Wiebbelling <mrodrigow@gmail.com>
2023-11-15 15:12:09 +0100

    Moving to springdoc-openapi-starter-webmvc-ui

--- src/main/java/com/marcelorodrigo/apidifference/config/SwaggerConfig.java
+++ src/main/java/com/marcelorodrigo/apidifference/config/SwaggerConfig.java
@@ -4,25 +4,14 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
-public class SwaggerConfig extends WebMvcConfigurationSupport {
+public class SwaggerConfig {
 
     @Bean
     public OpenAPI apiInfo() {
         return new OpenAPI()
                 .info(new Info().title("Scalable Web API")
                         .description("WAES Assignment"));
-    }
-
-    @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
